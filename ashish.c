e3fa76050ab1821de3fa2347405a9ab392c6d626
Ashish Aman <ashishaman@tradelab.in>
2023-01-14 20:43:52 +0530

    initial commit ashish

---
+++ ashish.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	char* string = "Ashish aman."; // Your name here. ^_^
+	char* string = "Ashish aman."; // Your name here. ^_^
+  char* string = "Sunny Arora."; // Your name here. ^_^
+	printf("%s\n", string);
+	return 0;
+}
