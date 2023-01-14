5bd53a18d6fd72077a345901419406c3b0305532
JSATANJ <arorasunny175@gmail.com>
2023-01-14 19:59:51 +0530

    practice commit

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main() {
-	char* string = "Param Siddharth."; // Your name here. ^_^
+void main() {
+	char* string = "Sunny Arora."; // Your name here. ^_^
 	printf("%s\n", string);
 	return 0;
 }
