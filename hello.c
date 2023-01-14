ca1efa7f212da3c40243d3bc672513121765c178
anamika242 <achamola@estee.com>
2023-01-14 21:19:37 +0530

    Made changes to hello.c with name

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
+	char* string = "Anamika Chamola"; // Your name here. ^_^
 	char* string = "Deepak Vishwakarma."; // Your name here. ^_^
 	char* string = "Lanke vamsi kiran."; // Your name here. ^_^
-  char* string = "Sunny Arora."; // Your name here. ^_^
+    char* string = "Sunny Arora."; // Your name here. ^_^
 	printf("%s\n", string);
 	return 0;
 }
