c3765ce8a6bed53b3e05a5eb27b5a474e3171844
Md. Touhidul Islam <touhidulislam1810@gmail.com>
2023-01-15 12:47:32 +0600

    My Name Touhid is added

--- hello.c
+++ hello.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	char* string = "Deepak Vishwakarma."; // Your name here. ^_^
-	char* string = "Lanke vamsi kiran."; // Your name here. ^_^
-  char* string = "Sunny Arora."; // Your name here. ^_^
+	char string = "Md. Touhidul Islam"; //Your name here. ^_^
 	printf("%s\n", string);
 	return 0;
 }
