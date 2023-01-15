099bd735501d9188153e4178e7d7e5892d1b44a9
MohiPre <smohanraj77777@gmail.com>
2023-01-15 12:47:45 +0000

    MyName

--- hello.c
+++ hello.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-	char* string = "Anamika Chamola"; // Your name here. ^_^
-	char* string = "Deepak Vishwakarma."; // Your name here. ^_^
-	char* string = "Lanke vamsi kiran."; // Your name here. ^_^
-  char* string = "Sunny Arora."; // Your name here. ^_^
-	char string = "Md. Touhidul Islam"; //Your name here. ^_^
+	
+	char* string =  "Mohan Raj S ";
 	printf("%s\n", string);
 	return 0;
 }
