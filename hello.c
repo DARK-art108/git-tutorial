51bf6bd632ac9592f3a550a066cb65caa5af65c6
sagicha11 <sweetyagicha11@gmail.com>
2023-01-15 12:30:57 +0100

    modified hello.c to see merge conflict

--- hello.c
+++ hello.c
@@ -2,6 +2,8 @@
 
 int main() {
 	char* string = "Param Siddharth."; // Your name here. ^_^
+	char* string1 = " - Sweetie"; // Your name here. ^_^
 	printf("%s\n", string);
+	printf("just to see the conflict :) %s\n",string1);
 	return 0;
 }
