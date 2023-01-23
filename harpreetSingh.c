c3bf05adb402fb0bca2dd100e4f8106f9b93e7f9
Singh <h.singh8149@gmail.com>
2023-01-23 15:17:43 +0100

    added assignment

---
+++ harpreetSingh.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /* char *string = "Param Siddharth.";    // Your name here. ^_^
+    char *string1 = " - Sweetie";         // Your name here. ^_^
+    char *string = "Anamika Chamola";     // Your name here. ^_^
+    char *string = "Deepak Vishwakarma."; // Your name here. ^_^
+    char *string = "Lanke vamsi kiran.";  // Your name here. ^_^
+    char *string = "Sunny Arora.";        // Your name here. ^_^
+    char *string = "Md. Touhidul Islam";   // Your name here. ^_^ */
+    char *strsingh = "Harpreet Singh"; // Your name here. ^_^
+    printf("%s\n", strsingh);
+    return 0;
+}
