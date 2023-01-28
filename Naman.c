74ce20def85170b9767a2dc5660641a48a561257
Naman Panchal <namanpanchal64@gmail.com>
2023-01-28 22:42:59 +0530

    added Naman.c file

---
+++ Naman.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  char string = "Naman"; //Your name here. ^_^
+	printf("%s\n", string);
+	printf("just to see the conflict :) %s\n",string1);
+	return 0;
+}
--- hello.c
+++ hello.c
@@ -9,6 +9,7 @@ int main() {
 	char* string = "Lanke vamsi kiran."; // Your name here. ^_^
   char* string = "Sunny Arora."; // Your name here. ^_^
 	char string = "Md. Touhidul Islam"; //Your name here. ^_^
+	char string = "Naman"; //Your name here. ^_^
 	printf("%s\n", string);
 	printf("just to see the conflict :) %s\n",string1);
 	return 0;
