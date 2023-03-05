2014b30c0cff6a5f132df598ed1e64f85a39ff3b
ShoaibAlam55 <zashoaib@gmail.com>
2023-03-05 14:08:34 +0530

    Added the hello C code for my name

---
+++ Shoaib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	char* string = "Shoaib Alam."; // Your name here. ^_^
+	char* string1 = " Shoaib"; // Your name here. ^_^
+	char* string = "Alam Shoaib"; // Your name here. ^_^
+	char* string = "Shoaib Alam."; // Your name here. ^_^
+	char* string = "Alam shoaib."; // Your name here. ^_^
+  char* string = "Shoaib"; // Your name here. ^_^
+	char string = "Md. Shoaib Alam"; //Your name here. ^_^
+	printf("%s\n", string);
+	printf("just to see the conflict :) %s\n",string1);
+	return 0;
+}
