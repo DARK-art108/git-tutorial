96c2cca617b6c68a1af5baca3416527d64b0d092
poulav <poulavbiswas@gmail.com>
2023-04-10 22:16:39 +0530

    Added inside a branch

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-	char string = "Poulav Biswas"; //Your name here. ^_^
+	char string = "Poulav Biswas"; //Your name here.
 	printf("%s\n", string);
 	printf("just to see the conflict :) %s\n",string);
 	return 0;
