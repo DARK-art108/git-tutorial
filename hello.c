c7fbccf58a891c7cf6c44f8a86855ef02b8476e3
poulav <poulavbiswas@gmail.com>
2023-04-10 22:14:39 +0530

    Name Updated

--- hello.c
+++ hello.c
@@ -2,14 +2,8 @@
 
 int main() {
 
-	char* string = "Param Siddharth."; // Your name here. ^_^
-	char* string1 = " - Sweetie"; // Your name here. ^_^
-	char* string = "Anamika Chamola"; // Your name here. ^_^
-	char* string = "Deepak Vishwakarma."; // Your name here. ^_^
-	char* string = "Lanke vamsi kiran."; // Your name here. ^_^
-  char* string = "Sunny Arora."; // Your name here. ^_^
-	char string = "Md. Touhidul Islam"; //Your name here. ^_^
+	char string = "Poulav Biswas"; //Your name here. ^_^
 	printf("%s\n", string);
-	printf("just to see the conflict :) %s\n",string1);
+	printf("just to see the conflict :) %s\n",string);
 	return 0;
 }
