1041af826a095f25063c71cc2ff355926ae7706a
Varaprasadjagu <varaprasadjagu@gmail.com>
2023-06-06 13:12:11 +0530

    Added my name and removed all other names

--- hello.c
+++ hello.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
 int main() {
-
-	char* string = "Param Siddharth."; // Your name here. ^_^
-	char* string1 = " - Sweetie"; // Your name here. ^_^
-	char* string = "Anamika Chamola"; // Your name here. ^_^
-	char* string = "Deepak Vishwakarma."; // Your name here. ^_^
-	char* string = "Lanke vamsi kiran."; // Your name here. ^_^
-  char* string = "Sunny Arora."; // Your name here. ^_^
-	char string = "Md. Touhidul Islam"; //Your name here. ^_^
+	char string = "Vara prasad Jagu"; //Your name here. ^_^
 	printf("%s\n", string);
 	printf("just to see the conflict :) %s\n",string1);
 	return 0;
